7a7eca5c07a34ea052fea234b98efe6455e8f926
Andrey <irotaev@gmail.com>
2017-03-21 06:32:02 +0300

    Правка дизайна и сайта

--- FE/src/app/app.component.ts
+++ FE/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit():void {
+    var script = document.createElement( "script" );
+    script.type = "text/javascript";
+    script.src = "js/main.js";
+    $("body").append(script);
+  }
+
 }
